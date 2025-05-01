d89b6c40b9059e32e52df5a3bd69c1c5dd16d129
Rahul Karthik <rahulkarthik@mac.lan>
2025-04-30 23:09:31 -0500

    Landing Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import { confirmSignUp } from 'aws-amplify/auth';
 
 Amplify.configure(awsAmplifyConfig);
 export default function Home() {
-  console.log(process.env.NEXT_PUBLIC_aws_project_region)
   const [email,setEmail]=useState("")
   const [password,setPassword]=useState("")
   const [code,setCode]=useState("")
@@ -27,11 +26,13 @@ export default function Home() {
     });
     console.log(isSignUpComplete,nextStep)
   }
-
+  function handleClick(){
+    console.log("Clicked Creat Account")
+  }
 
   return (
     <>
-      <div>
+      {/* <div>
         <label htmlFor="email">Email:</label>
         <input type="text" id="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
         <label htmlFor="password">Password:</label>
@@ -39,6 +40,24 @@ export default function Home() {
         <button onClick={()=>handleSubmit()}>Submit</button>
         <input type="text" id="confirmationCode" value={code} onChange={(e)=>setCode(e.target.value)} />Code:
         <button onClick={()=>final()} >Confirm</button>
+      </div> */}
+      <div className='container'>
+        <div className='intro'>
+          <h1>NutriSmart</h1>
+          <h2>Your Personal Nutrition Assistance</h2>
+          <p>Track nutrition, get personalized meal recommendations, and achieve your health goals with AI-powered guidance.</p>
+          <ul>
+            <li><p>Personalized nutrition tracking</p></li>
+            <li><p>AI-powered meal recommendations</p></li>
+            <li><p>Smart meal planning assistant</p></li>
+          </ul>
+        </div>
+        <div className='entry'>
+          <h1>Get Started Today</h1>
+          <h2>Join thousands of users who have transformed their nutrition habits with NutriSmart's personalized guidance.</h2>
+          <button onClick={handleClick}>Create Your Account</button>
+          <p>Do you have an account? <a href="">Sign in</a></p>
+        </div>
       </div>
     </>
   );
